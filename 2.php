1e493c29681576f118b9e3722701db26be1c4893
AsafeRoger <134728635+AsafeRoger@users.noreply.github.com>
2023-12-01 13:59:44 -0300

    2.php

---
+++ 2.php
@@ -0,0 +1,12 @@
+$sete_maravilhas[7] = [
+    "Cristo Redentor",
+    "Chichén Itzá",
+    "Coliseu",
+    "Muralha da China",
+    "Petra",
+    "Machu Picchu",
+    "Taj Mahal"
+];
+for($i=0; $i < 7; $i++){
+    print($i + 1 . ": " . $sete_maravilhas[$i] . "\n");
+}
